2de135d7344cdd46b33c69952756877642bbf208
Adolfo Custidiano <adolfoecs@gmail.com>
2021-11-15 10:50:23 +0000

    adding more complex test cases scenarios

--- src/test/java/robots/RobotMissionControlIntegrationTest.java
+++ src/test/java/robots/RobotMissionControlIntegrationTest.java
@@ -70,4 +70,46 @@ public class RobotMissionControlIntegrationTest {
                 .contains(Position.builder().x(2).y(3).orientation(Orientation.S).isLost(false).build());
     }
 
+    @Test
+    public void shouldReturnRightPositionsWhenThereIsScentRobots() {
+        //Given
+        robotMissionControl.setFieldSize("5 3");
+        robotMissionControl.addRobot("1 1 E", "RFRFRFRF");
+        robotMissionControl.addRobot("3 2 N", "FRRFLLFFRRFLL");
+        robotMissionControl.addRobot("0 3 W", "LLFFFLFLFL");
+        robotMissionControl.addRobot("3 3 N", "FLLFF");
+
+        //When
+        robotMissionControl.execute();
+
+        //Then
+        assertThat(robotMissionControl.results).hasSize(4);
+        assertThat(robotMissionControl.results)
+                .contains(Position.builder().x(1).y(1).orientation(Orientation.E).build())
+                .contains(Position.builder().x(3).y(3).orientation(Orientation.N).isLost(true).build())
+                .contains(Position.builder().x(2).y(3).orientation(Orientation.S).isLost(false).build())
+                .contains(Position.builder().x(3).y(1).orientation(Orientation.S).isLost(false).build());
+    }
+
+    @Test
+    public void shouldReturnRightPositionsWhenThereAreMoreScentsRobots() {
+        //Given
+        robotMissionControl.setFieldSize("5 3");
+        robotMissionControl.addRobot("1 1 E", "RFRFRFRF");
+        robotMissionControl.addRobot("3 2 N", "FRRFLLFFRRFLL");
+        robotMissionControl.addRobot("4 3 N", "FLLFF");
+        robotMissionControl.addRobot("3 3 N", "FRFLFR");
+
+        //When
+        robotMissionControl.execute();
+
+        //Then
+        assertThat(robotMissionControl.results).hasSize(4);
+        assertThat(robotMissionControl.results)
+                .contains(Position.builder().x(1).y(1).orientation(Orientation.E).build())
+                .contains(Position.builder().x(3).y(3).orientation(Orientation.N).isLost(true).build())
+                .contains(Position.builder().x(4).y(3).orientation(Orientation.N).isLost(true).build())
+                .contains(Position.builder().x(4).y(3).orientation(Orientation.E).isLost(false).build());
+    }
+
 }
